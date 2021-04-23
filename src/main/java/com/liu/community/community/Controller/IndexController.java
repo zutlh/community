d3c947b5eacb04755a3a0108f7e32fad38a7ea7b
apple <liuhao19971212@163.com>
2021-04-23 22:33:45 +0800

    update index.html

--- src/main/java/com/liu/community/community/Controller/IndexController.java
+++ src/main/java/com/liu/community/community/Controller/IndexController.java
@@ -1,8 +1,13 @@
 package com.liu.community.community.Controller;
 
 import com.alibaba.fastjson.JSON;
+import com.liu.community.community.dto.QuestionDTO;
+import com.liu.community.community.mapper.QuestionMapper;
 import com.liu.community.community.mapper.UserMapper;
+import com.liu.community.community.model.Question;
 import com.liu.community.community.model.User;
+import com.liu.community.community.service.QuestionService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +16,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request,
+                       Model model
+    ) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
@@ -31,8 +41,10 @@ public class IndexController {
                     break;
                 }
             }
-            return "index";
         }
+
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
 
 
---
+++ src/main/java/com/liu/community/community/service/QuestionService.java
@@ -0,0 +1,36 @@
+package com.liu.community.community.service;
+
+import com.liu.community.community.dto.QuestionDTO;
+import com.liu.community.community.mapper.QuestionMapper;
+import com.liu.community.community.mapper.UserMapper;
+import com.liu.community.community.model.Question;
+import com.liu.community.community.model.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+    @Autowired
+    private UserMapper userMapper;
+
+    public List<QuestionDTO> list() {
+        List<Question> questions = questionMapper.list();
+        ArrayList<QuestionDTO> questionDTOArrayList = new ArrayList<>();
+        for (Question question : questions) {
+           User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question,questionDTO);//把question对象的属性快速copy到questionDTO中
+            questionDTO.setUser(user);
+            questionDTOArrayList.add(questionDTO);
+        }
+
+        return questionDTOArrayList;
+    }
+}
